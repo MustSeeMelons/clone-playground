207c258bd266d7c949f8de78963e89b56b961d75
strautins <strautins.janis@gmail.com>
2024-08-04 12:12:03 +0300

    Move all styles to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    // container mx-auto
     <>
-      {/* columns-2 flex  */}
-      <div>
-        {/* className="self-end" */}
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="columns-2 flex justify-center">
+        <a className="self-end" href="https://vitejs.dev" target="_blank">
+          {/* XXX will-change-filter was in the CSS */}
+          <img
+            src={viteLogo}
+            className="logo transition-shadow p-6 h-24 hover:drop-shadow-logoVite"
+            alt="Vite logo"
+          />
         </a>
-        {/* className="self-start" */}
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a className="self-start" href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="motion-safe:animate-logoSpin transition-shadow p-6 h-24 hover:drop-shadow-logoReact"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      {/* p-8 */}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="p-8">
+        <button
+          className="btn-primary"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-400">
         Click on the Vite and React logos to learn more
       </p>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    /* XXX We could extend all and make it 1:1 to the source, but that would be futile */
+    extend: {
+      dropShadow: {
+        logoVite: "0 0 2em #646cffaa",
+        logoReact: "0 0 2em #61dafbaa",
+      },
+      keyframes: {
+        logoSpin: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        logoSpin: "logoSpin infinite 20s linear",
+      },
+      fontFamily: {
+        font: [
+          "Inter",
+          "system-ui",
+          "Avenir",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
